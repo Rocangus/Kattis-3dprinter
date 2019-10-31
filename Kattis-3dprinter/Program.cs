1917afb0bf3d7d57d8b3803dde8e0c42faec5e4e
Rocangus <hunter_sniper112@hotmail.com>
2019-10-31 16:23:44 +0100

    Initial setup

---
+++ Kattis-3dprinter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Kattis_3dprinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var targetStatues = Int32.Parse(input);
+            Console.WriteLine(DaysRequired(targetStatues));
+        }
+
+        private static int DaysRequired(int target)
+        {
+            
+        }
+    }
+}
