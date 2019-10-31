f58fe3b66822570ffbc09daa3b5da77546c321e6
Rocangus <hunter_sniper112@hotmail.com>
2019-10-31 16:45:22 +0100

    Finished and Accepted

--- Kattis-3dprinter/Program.cs
+++ Kattis-3dprinter/Program.cs
@@ -2,18 +2,30 @@
 
 namespace Kattis_3dprinter
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             var input = Console.ReadLine();
-            var targetStatues = Int32.Parse(input);
+            var targetStatues = int.Parse(input);
             Console.WriteLine(DaysRequired(targetStatues));
         }
 
-        private static int DaysRequired(int target)
+        public static int DaysRequired(int target)
         {
-            
+            int days = int.MaxValue;
+
+            for (int i = 0; i <= target; i++)
+            {
+                int printers = (int)Math.Pow(2, i);
+                int attemptDays = i + (int)Math.Ceiling((double)target / printers);
+                if (attemptDays >= days)
+                {
+                    break;
+                }
+                days = attemptDays;
+            }
+            return days;
         }
     }
 }
---
+++ Kattis-3dprinterTests/ProgramTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kattis_3dprinter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kattis_3dprinter.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void DaysRequiredTest()
+        {
+            //Arrange
+            int input = 1;
+            int expected = 1;
+
+            //Act
+            int actual = Program.DaysRequired(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void DaysRequiredTest2()
+        {
+            //Arrange
+            int input = 5;
+            int expected = 4;
+
+            //Act
+            int actual = Program.DaysRequired(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
